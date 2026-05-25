5c49d27802f499021b4a75046323ff6d620f3eda
Caepena <caepenafiel@gmail.com>
2026-05-25 18:54:52 -0300

    Calculadora de Investimentos 01

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  @Output()
+  calculate = new EventEmitter<{
+    initialInvestment: number;
+    annualInvestment: number;
+    duration: number;
+    expectedReturn: number;
+  }>();
 
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredDuration = '10';
+  enteredExpectedReturn = '5';
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      annualInvestment: +this.enteredAnnualInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+    });
+  }
 }
